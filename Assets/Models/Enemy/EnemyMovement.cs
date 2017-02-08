d4e81db0d69f1878280de627fe47e02d4ab8cd87
Javier Lores <lores.javier@gmail.com>
2017-02-07 23:56:51 -0500

    Added initial stormtrooper enemy

---
+++ Assets/Models/Enemy/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    NavMeshAgent nav;
+    Animator anim;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+            nav.SetDestination(player.position);
+        else
+            nav.enabled = false;
+
+        // Animate the player.
+        Animate();
+    }
+
+
+    void Animate()
+    {
+        // Tell the animator whether or not the player is walking.
+        anim.SetBool("IsWalking", true);
+    }
+}
---
+++ Assets/Models/Enemy/LaserBlast/LaserBlast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBlast : MonoBehaviour {
+
+    public int attackDamage = 10;
+
+    GameObject player;
+    PlayerHealth playerHealth;
+
+    private void Awake()
+    {
+        this.gameObject.GetComponentInChildren<AudioSource>().Play();
+    }
+
+    void Start () {
+        player = GameObject.Find("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+
+    void Update()
+    {
+        gameObject.transform.position += 25.0f * Time.smoothDeltaTime * gameObject.transform.forward;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(player))
+            playerHealth.TakeDamage(attackDamage);
+    }
+}
