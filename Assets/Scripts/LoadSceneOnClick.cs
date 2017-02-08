eb4fc7ffa1db38a61e82d735c2513d4a8451fccb
blakeklasing <blakeklasing@gmail.com>
2017-02-08 15:06:01 -0500

    Some main menu stuff!

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
